964583322f7c4ccec24d8938943ed676315ed436
Phattadol358 <phattadol@outlook.com>
2024-07-31 10:25:50 +0700

    modifued surname variable and sed name and surname text defualt

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="q-pa-md" style="max-width: 400px">
-
     <q-form
       @submit="onSubmit"
       @reset="onReset"
@@ -10,11 +9,19 @@
         filled
         v-model="name"
         label="Your name *"
-        hint="Name and surname"
+        hint="Name"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
-      />
+        :rules="[ val => val && val.length > 0 || 'Please type your name']"
 
+      />
+      <q-input
+        filled
+        v-model="surname"
+        label="Your surname *"
+        hint="surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type your surname']"
+        />
       <q-input
         filled
         type="number"
@@ -26,15 +33,12 @@
           val => val > 0 && val < 100 || 'Please type a real age'
         ]"
       />
-
       <q-toggle v-model="accept" label="I accept the license and terms" />
-
       <div>
         <q-btn label="Submit" type="submit" color="primary"/>
         <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
-
   </div>
 </template>
 
@@ -46,12 +50,14 @@ export default {
   setup () {
     const $q = useQuasar()
 
-    const name = ref(null)
+    const name = ref('พัทธดนย์ ')
+    const surname = ref('แก้วกัลยา')
     const age = ref(null)
     const accept = ref(false)
 
     return {
       name,
+      surname,
       age,
       accept,
 
